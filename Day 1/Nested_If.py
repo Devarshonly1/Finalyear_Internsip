8f1893398f3833b66a988d2ff848ef3ec7e2b72c
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-28 13:58:27 +0530

    Add files via upload

---
+++ Day 1/Nested_If.py
@@ -0,0 +1,38 @@
+# Nested if...
+"""
+if condition :
+
+      if condition :
+             statement
+
+             else :
+                  statement
+
+             else :
+                  statement
+
+"""
+
+Num=float(input("Enter value of number : "))
+
+if Num >=0 :
+    if Num == 0 :
+        
+        print("Number is Zero ")
+
+    else :
+
+        print("Number is Positive")
+
+else :
+       
+    print("Number is Negative")
+
+  
+
+ 
+
+
+
+ 
+ 
