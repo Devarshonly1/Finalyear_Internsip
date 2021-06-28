3e9755fce5e22e5275edbcbc2b704633d040d87e
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-28 14:12:39 +0530

    Add files via upload

---
+++ Day 1/Ladder_if.py
@@ -0,0 +1,33 @@
+ # Ladder if/else
+
+"""
+if condition :
+    statement
+
+    elif condition :
+        statement
+
+        elif condition :
+            statement
+
+            else condition :
+                statement
+"""
+
+a=int (input("Enter value of A : "))
+b=int (input("Enter value of B : "))
+c=int (input("Enter value of C : "))
+
+if a>b :
+
+    print(" A is Largest number ")
+
+elif b>c :
+
+    print(" B is Largest number ")
+
+else :
+    
+    print(" C is Largest number ")
+    
+
