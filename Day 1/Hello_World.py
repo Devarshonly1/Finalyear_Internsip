372fd2634103a60e8ac323bae668790ba3cf8717
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-28 13:43:52 +0530

    Add files via upload

---
+++ Day 1/Hello_World.py
@@ -0,0 +1,15 @@
+a=("Hello World of  Python")
+
+b=int(input("Enter Value of  b : "))
+c=int(input("Enter value of  c : "))
+
+d=b+c
+e=b*c
+f=b/c
+g=b-c
+
+print(a)
+print("Addition : ", d )
+print( "Multiplication : ", e  )
+print("Division : ", f  )
+print( "Substraction :", g )
---
+++ Day 1/If_Programs.py
@@ -0,0 +1,29 @@
+'''
+1. Simple if
+
+             if condition :
+                statement
+  
+2. if/else
+
+             if condition :
+                statement
+
+                              else :
+                                statement
+                                
+'''
+
+a=int(input("Enter value of A : "))
+b=int(input("Enter value of B : "))
+
+
+if a>b :
+
+    print(" A is Greater number ")
+
+else :
+        
+        print(" B is Greater number")
+
+
