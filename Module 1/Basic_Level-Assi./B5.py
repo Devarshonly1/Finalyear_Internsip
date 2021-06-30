b48c5283baa38fa9bdda09a7deff3673c5077e8c
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-30 12:38:25 +0530

    Add files via upload

---
+++ Module 1/Basic_Level-Assi./B5.py
@@ -0,0 +1,17 @@
+# Number is Positive/Negative/Zero.
+
+n=int(input("Enter value of N : "))
+
+if n>=0 :
+
+    if n==0 :
+
+        print("Number is Zero : ",n )
+
+    else :
+
+        print("Number is Positive : ",n)
+
+else :
+    print("Number is Negative : ",n)
+        
