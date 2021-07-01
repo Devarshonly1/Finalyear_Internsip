0641a3ad013f1ddc1724ad67e67f01f95c40862d
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-07-01 23:09:28 +0530

    Add files via upload

---
+++ Day 3/List_As_Stack.py
@@ -0,0 +1,31 @@
+print(" #List As Stack")
+L=[]
+
+#Adding Numbers to the List.
+
+L.append(10)
+print(L)
+L.append(20)
+print(L)
+L.append(30)
+print(L)
+L.append(40)
+print(L)
+L.append(50)
+print(L)
+L.append(60)
+print(L)
+
+# Removing Numbers from the List
+L.pop()
+print(L)
+L.pop()
+print(L)
+L.pop()
+print(L)
+L.pop()
+print(L)
+L.pop()
+print(L)
+L.pop()
+print(L)
