7ff9ec6c090178a399dd2a0064024a174e6af00b
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-07-01 22:55:39 +0530

    Add files via upload

---
+++ Day 3/List_Demo.py
@@ -0,0 +1,27 @@
+L=[1,2,3,4.4,2.2,False,3.3,"hello world",True,1,2,3,"Python",False]
+
+print(L)
+L.append(100)
+print(L)
+L1=L.copy()
+print(L1)
+print(L.count(1))
+L2=[200,300,400]
+L.extend(L2)
+print(L)
+L.pop()
+print(L)
+L.reverse()
+print(L)
+L3=[8,3,6,2,7,1,9,4,5,10]
+L3.sort()
+print(L3)
+L3.sort(reverse=True)
+print(L3)
+L4=["Hemil","Himen","Dixant","Jaimil","Meet","Abhi","Pranav","Ayush","Smit"]
+L4.sort()
+print(L4)
+
+
+
+
