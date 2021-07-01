ea9ac4658d9e8fcc1e815ca67689edda959b5cc7
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-07-01 21:46:21 +0530

    Add files via upload

---
+++ Day 2/String_Demo.py
@@ -0,0 +1,18 @@
+s="Hello World Of PYTHON"
+
+print(s)
+print(s.capitalize())
+print(s.casefold())
+print(s.lower())
+print(s.upper())
+print(s.center(40,"*"))
+print(s.count("World"))
+print(s.endswith("ON"))
+print(s.startswith("Of"))
+print(len(s))
+print(min(s))
+print(max(s))
+print("Hello World Of PYTHON".replace("0","w"))
+print(s.swapcase())
+
+
